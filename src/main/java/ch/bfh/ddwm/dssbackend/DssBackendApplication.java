39bdc8619216c625fe218771bf74a239fe743ff8
Janic Scherer <janic.scherer@students.bfh.ch>
2026-03-29 16:43:23 +0200

    Initial Spring Boot setup

---
+++ src/main/java/ch/bfh/ddwm/dssbackend/DssBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.bfh.ddwm.dssbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DssBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DssBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bfh/ddwm/dssbackend/DssBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bfh.ddwm.dssbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DssBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
