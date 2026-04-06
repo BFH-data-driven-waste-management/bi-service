9e786ba6c322b31423298177a7b30265b7e2e251
Janic Scherer <janic.scherer@students.bfh.ch>
2026-04-07 00:08:09 +0200

    temp cross origin annotation for bin list controller

--- src/main/java/ch/bfh/ddwm/dssbackend/binlist/BinListController.java
+++ src/main/java/ch/bfh/ddwm/dssbackend/binlist/BinListController.java
@@ -1,6 +1,7 @@
 package ch.bfh.ddwm.dssbackend.binlist;
 
 import ch.bfh.ddwm.dssbackend.binlist.dto.BinListResponse;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200") // TODO move to central config
 @RequestMapping("/api/bins/binlist")
 public class BinListController {
 
